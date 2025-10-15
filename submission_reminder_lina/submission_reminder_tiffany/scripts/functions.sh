39d22a17385e71a8a04133774695e053e399956e
Tiffany Lina <t.turate@alustudent.com>
2025-10-15 09:00:10 -0700

    Keep only create_environment.sh, remove old files

--- submission_reminder_lina/submission_reminder_tiffany/scripts/functions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-check_pending_submissions() {
-  while IFS=',' read -r name status; do
-    if [ "$status" != "submitted" ]; then
-      echo "Reminder: $name $REMINDER_MESSAGE"
-    fi
-  done < ../data/submissions.txt
-}
--- submission_reminder_lina/submission_reminder_tiffany/scripts/startup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Starting Submission Reminder App..."
-bash ./reminder.sh
